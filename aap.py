8cdee9e05d6052b96cecb4e555a44218dffe4580
Megachinnu <bajameganath@gmail.com>
2026-04-01 06:57:56 +0530

    Added some code

--- aap.py
+++ aap.py
@@ -1,3 +1,14 @@
 Hi app.py
 how is the GIT hub class
 we are happy to learn this course
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
